6abbe3bd1e8cd2e5287341718acef999f8fcaa17
maske <maskeee@gmail.com>
2018-02-23 00:44:25 -0300

    se agrego about

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>Test de nuxt-test</h1>
+      <p>Probando nuxt.js con vue y netlify</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
